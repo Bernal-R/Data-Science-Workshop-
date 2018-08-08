1f33a88a9e7a95845c42687f78c0e1c1785221db
Bernal Rojas <berrojas45@gmail.com>
2018-08-08 17:57:10 -0600

    Codigo correcto

--- Ejemplo_Automotriz/ArbolDesicion.R
+++ Ejemplo_Automotriz/ArbolDesicion.R
@@ -1,43 +1,21 @@
-#=================== 1.Importamos librerias ===================
-library(caret)
-library(rpart.plot)
+library(C50)
 library(rpart)
+library(rpart.plot) 
+data(churn); # carga tablas
 
-#=================== 2.Importamos set de datos ===================
-setwd("/Users/bernalrojas/Documents/ArbolDesicion")
-car_df = read.csv("database.csv", header = TRUE, dec = ".", sep = ",")
+Variables      <-c(4,7,16,19,17,20)               # variables elegidas
+Entrenamiento  <-churnTrain[,Variables]           # tabla entrenamiento
+Test           <-churnTest [,Variables]           # tabla  Test
 
 
-#=================== 3.Separamos Set de datos para Entrenamiento y Prueba ===================
-set.seed(3033) #Punto de inicio (No aleatorio)
-intrain <- createDataPartition(y = car_df$acceptation, p= 0.7, list = FALSE) # 70% - 30%
-training <- car_df[intrain,]
-testing <- car_df[-intrain,]
-
-#Revisar dimensiones para asegurarse que la partici??n fue exitosa
-dim(training); dim(testing);
-
-#=================== PreEntrenamiento ===================
-#Nota: Este paso se realiza por buena costumbre, mas no es necesario del todo.
-anyNA(car_df)     #Revisamos que no hayan valores perdidos que nos den porblemas luego
-summary(car_df)   #Est??disticas descriptivas de los datos
-
-#=================== 4.Entrenamiento ===================
-trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3) #Estructura del arbol, estamos usando el m??todo para Validaci??n Cruzada
-dtree_fit <- train(acceptation ~., data = training, method = "rpart", 
-                   parms = list(split = "information"),
-                   trControl=trctrl,
-                   tuneLength = 10) #Entrenamiento 
-dtree_fit #Mostramos el modelo entrenado
-
-prp(dtree_fit$finalModel, box.palette = "Blues", tweak = 1.2) #Generar modelo gr??fico
-
-#=================== 5.Prediccion ===================
-#Predecimos para 1 auto
-testing[1,] #Cargamos la fila 1
-predict(dtree_fit, newdata = testing[1,]) #Predecimos 
-
-#Probamos para todos los autos
-test_pred <- predict(dtree_fit, newdata = testing) #Predecimos
-confusionMatrix(test_pred, testing$acceptation )   #Revisar Accuracy 
-
+ # PASO 2:   Crea Arbol de Decision
+# ---------------------------------------------------------------------------
+ModeloArbol<-rpart(churn ~ .,data=Entrenamiento,parms=list(split="information"))
+ # PASO 3:  Predice Desafiliaci??n en datos de TEST
+# ---------------------------------------------------------------------------
+Prediccion <- predict(ModeloArbol, Test,type="class") # Prediccci??n en Test
+MC         <- table(Test[, "churn"],Prediccion) # Matriz de Confusi??n
+ # PASO 4: Crea Grafico
+# ---------------------------------------------------------------------------
+rpart.plot(ModeloArbol, type=1, extra=100,cex = .7,
+           box.col=c("#D7DADB", "#6DBCDB")[ModeloArbol$frame$yval])
