80efd1b79a7b6f83cf27087ecda095cc78b44621
Bernal Rojas <berrojas45@gmail.com>
2018-08-08 18:00:18 -0600

    .

--- Ejemplo_Telefonica/ArbolTelefonica.R
+++ Ejemplo_Telefonica/ArbolTelefonica.R
@@ -1,30 +1,20 @@
-# Predecir Perdida de Clientes con Arbol de Decision
-
-# PASO 1:   Carga Package y Set de datos
-# ---------------------------------------------------------------------------
 library(C50)
 library(rpart)
 library(rpart.plot) 
-library(caret)
+data(churn); # carga tablas
 
-setwd("/Users/bernalrojas/Documents")
-clientes = read.csv("DataBase_clients.csv", header = TRUE, dec = ".", sep = ",")
+Variables      <-c(4,7,16,19,17,20)               # variables elegidas
+Entrenamiento  <-churnTrain[,Variables]           # tabla entrenamiento
+Test           <-churnTest [,Variables]           # tabla  Test
 
-intrain <- createDataPartition(y = clientes$churn, p= 0.63, list = FALSE) # 63% - 37%
-Entrenamiento <- clientes[intrain,]
-Test <- clientes[-intrain,]
 
 # PASO 2:   Crea Arbol de Decision
 # ---------------------------------------------------------------------------
-ModeloArbol<-rpart(clientes ~ .,data=Entrenamiento,parms=list(split="information"))
-
-
-# PASO 3:  Predice Desafiliación en datos de TEST
+ModeloArbol<-rpart(churn ~ .,data=Entrenamiento,parms=list(split="information"))
+# PASO 3:  Predice Desafiliaci??n en datos de TEST
 # ---------------------------------------------------------------------------
-Prediccion <- predict(ModeloArbol, Test,type="class") # Prediccción en Test
-MC         <- table(Test[, "churn"],Prediccion) # Matriz de Confusión
-
-
+Prediccion <- predict(ModeloArbol, Test,type="class") # Prediccci??n en Test
+MC         <- table(Test[, "churn"],Prediccion) # Matriz de Confusi??n
 # PASO 4: Crea Grafico
 # ---------------------------------------------------------------------------
 rpart.plot(ModeloArbol, type=1, extra=100,cex = .7,
