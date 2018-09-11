cd05e95a539b80be05464ce97ec0e7b045ad0391
Bernal Rojas <berrojas45@gmail.com>
2018-09-11 17:41:37 -0600

    Update

--- Ejemplo_Telefonica/ArbolTelefonica.R
+++ Ejemplo_Telefonica/ArbolTelefonica.R
@@ -3,18 +3,19 @@ library(rpart)
 library(rpart.plot) 
 data(churn); # carga tablas
 
-Variables      <-c(4,7,16,19,17,20)               # variables elegidas
+Variables      <-c(4,7,16,19,17,20)               # variables elegidas del set de datos
 Entrenamiento  <-churnTrain[,Variables]           # tabla entrenamiento
 Test           <-churnTest [,Variables]           # tabla  Test
-
 
 # PASO 2:   Crea Arbol de Decision
 # ---------------------------------------------------------------------------
 ModeloArbol<-rpart(churn ~ .,data=Entrenamiento,parms=list(split="information"))
-# PASO 3:  Predice Desafiliaci??n en datos de TEST
+
+# PASO 3:  Predice Desafiliacion en datos de TEST
 # ---------------------------------------------------------------------------
-Prediccion <- predict(ModeloArbol, Test,type="class") # Prediccci??n en Test
-MC         <- table(Test[, "churn"],Prediccion) # Matriz de Confusi??n
+Prediccion <- predict(ModeloArbol, Test[-6],type="class") # Predicccion en Test
+Prediccion
+
 # PASO 4: Crea Grafico
 # ---------------------------------------------------------------------------
 rpart.plot(ModeloArbol, type=1, extra=100,cex = .7,
