0cb014e5ec6427dea16094d660b3560d2daf01e5
Bernal Rojas <berrojas45@gmail.com>
2018-09-12 09:27:32 -0600

    Update

--- Ejemplo_Automotriz/ArbolDesicion.R
+++ Ejemplo_Automotriz/ArbolDesicion.R
@@ -1,22 +1,60 @@
-library(C50)
+#=================== 1.Importamos librerias ===================
+library(caret)
+library(rpart.plot)
 library(rpart)
-library(rpart.plot) 
-data(churn); # carga tablas
 
-Variables      <-c(4,7,16,19,17,20)               # variables elegidas del set de datos
-Entrenamiento  <-churnTrain[,Variables]           # tabla entrenamiento
-Test           <-churnTest [,Variables]           # tabla  Test
+#=================== 2.Importamos set de datos ===================
+download.file(url = "https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data", destfile = "car.data")
+car_df <- read.csv("car.data", sep = ',', header = FALSE)
 
-# PASO 2:   Crea Arbol de Decision
-# ---------------------------------------------------------------------------
-ModeloArbol<-rpart(churn ~ .,data=Entrenamiento,parms=list(split="information"))
+#=================== 3.Separamos Set de datos para Entrenamiento y Prueba ===================
+set.seed(3033) #Punto de inicio (No aleatorio)
+intrain <- createDataPartition(y = car_df$V7, p= 0.7, list = FALSE) # 70% - 30%
+training <- car_df[intrain,]
+testing <- car_df[-intrain,]
 
-# PASO 3:  Predice Desafiliacion en datos de TEST
-# ---------------------------------------------------------------------------
-Prediccion <- predict(ModeloArbol, Test[-6],type="class") # Predicccion en Test
-Prediccion
+#Revisar dimensiones para asegurarse que la partici??n fue exitosa
+dim(training); dim(testing);
 
-# PASO 4: Crea Grafico
-# ---------------------------------------------------------------------------
-rpart.plot(ModeloArbol, type=1, extra=100,cex = .7,
-           box.col=c("#D7DADB", "#6DBCDB")[ModeloArbol$frame$yval])
+#=================== PreEntrenamiento ===================
+#Nota: Este paso se realiza por buena costumbre, mas no es necesario del todo.
+anyNA(car_df)     #Revisamos que no hayan valores perdidos que nos den porblemas luego
+summary(car_df)   #Est??disticas descriptivas de los datos
+
+#=================== 4.Entrenamiento ===================
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3) #Estructura del arbol, estamos usando el m??todo para Validaci??n Cruzada
+set.seed(3333)
+dtree_fit <- train(V7 ~., data = training, method = "rpart", 
+                   parms = list(split = "information"),
+                   trControl=trctrl,
+                   tuneLength = 10) #Entrenamiento 
+dtree_fit #Mostramos el modelo entrenado
+
+prp(dtree_fit$finalModel, box.palette = "Reds", tweak = 1.2) #Geramos el arbol gr??ficamente
+
+#=================== 5.Prediccion ===================
+#Predecimos para 1 auto
+testing[1,] #Cargamos la fila 1
+predict(dtree_fit, newdata = testing[1,]) #Predecimos 
+
+#Probamos para todos los autos
+test_pred <- predict(dtree_fit, newdata = testing) #Predecimos
+confusionMatrix(test_pred, testing$V7 )   #Revisar Accuracy 
+
+
+#=================== Alternativa a Prediccion Basada en Coeficientes de Gini ===================
+#Nota: Esta es una alternativa m??s precisa para realizar la predicci??n del paso anterior, mas no es necesaria del todo.
+
+#* Entrenamiento con Coficiente de Gini *
+set.seed(3333)
+dtree_fit_gini <- train(V7 ~., data = training, method = "rpart",
+                        parms = list(split = "gini"),
+                        trControl=trctrl,
+                        tuneLength = 10) #Entrenamiento basado en Coeficiente de Gini
+dtree_fit_gini #Ver modelo entrenado
+
+prp(dtree_fit_gini$finalModel, box.palette = "Blues", tweak = 1.2) #Generar modelo gr??fico
+
+# * Prediccion con Coficiente de Gini *
+test_pred_gini <- predict(dtree_fit_gini, newdata = testing)
+confusionMatrix(test_pred_gini, testing$V7 )  #Revisar Accuracy
